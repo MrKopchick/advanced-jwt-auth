0392e095b059508ba76d5c504e7d317f25220765
Illya <145700600+MrKopchick@users.noreply.github.com>
2025-04-24 17:18:13 +0300

    complete registration

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,7 +1,20 @@
+const userService = require('../service/user.service');
+
 class UserController {
     async registration(req, res) {  
         try{
+            const {email, password} = req.body;
+            console.log("Request body:", req.body);
 
+            if (!email || !password) {
+                return res.status(400).json({ message: 'Email and password are required' });
+            }
+            
+            const userData = await userService.registration(email, password);
+
+            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true});
+            
+            return res.json(userData);
         }catch(e){
             console.log("[REGISTRATION ERROR] ", e);
             res.status(500).json({message: 'Registration error'});
