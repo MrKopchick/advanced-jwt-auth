bdd9b91b33ded7719c267ecaee94c3e2c852f711
Illya <145700600+MrKopchick@users.noreply.github.com>
2025-04-24 17:17:58 +0300

    change db configuration

--- backend/src/config/db.config.js
+++ backend/src/config/db.config.js
@@ -1,46 +1,16 @@
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
 
-const uri = process.env.MONGODB_URI 
-  ? process.env.MONGODB_URI 
-  : (() => { 
-      console.error('[MongoDB] URI not provided'); 
-      process.exit(1); 
-    })();
+const mongoose = require('mongoose');
 
-const options = {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    },
-};
-
-let client;
-let isConnected = false;
 
 const connectToMongoDB = async () => {
-    if (!client) {
-        client = new MongoClient(uri, options);
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('[MongoDB] Connected via Mongoose');
+    } catch (error) {
+        console.error('[MongoDB Connection Error]', error);
+        throw error;
     }
-
-    if (!isConnected) {
-        try {
-            await client.connect();
-            isConnected = true;
-            console.log('[MongoDB] Connected');
-        } catch (err) {
-            throw new Error('[MongoDB] Connection failed: ' + err.message);
-        }
-    }
-    return client;
 };
 
-const getMongoClient = () => {
-    if (!isConnected) {
-        throw new Error('[MongoDB] Client not initialized');
-    }
-    return client;
-};
-
-module.exports = { connectToMongoDB, getMongoClient };
+module.exports = { connectToMongoDB };
