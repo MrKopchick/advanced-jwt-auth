0968f36d7a2dd8c14b8bce4c59b6bf08ade43787
Illya <145700600+MrKopchick@users.noreply.github.com>
2025-04-24 17:18:48 +0300

    fix syntax error

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -2,24 +2,23 @@ const {Schema, model} = require('mongoose');
 
 const UserSchema = new Schema({
     email:{
-        email:{
-            type:String,
-            required:true,
-            unique:true
-        },
-        email:{
-            type:String,
-            required:true,
-        },
-        isActivated:{
-            type:Boolean,
-            default:false,
-            required:true,
-        },
-        activationLink:{
-            type:String,
-        },
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    isActivated:{
+        type:Boolean,
+        default:false,
+        required:true,
+    },
+    activationLink:{
+        type:String,
     }
+    
 });
 
 module.exports = model('User', UserSchema);
