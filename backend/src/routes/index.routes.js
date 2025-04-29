f7d053c50686fb619d9ee7323c7b52ea425eeae5
Illya <illakoroluk62@gmail.com>
2025-04-29 23:08:31 +0300

    connect authMiddleware to routes

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const UserController = require('../controllers/user.controller');
 const router = new Router();
 const {body} = require('express-validator');
+const authMiddleware = require('../middleware/auth.middleware');
 
 router.post('/auth/registration', body('email').isEmail(), body('password').isLength({min:5, max: 32}), UserController.registration);
 router.post('/auth/login', UserController.login);
@@ -10,6 +11,6 @@ router.post('/auth/logout', UserController.logout);
 router.get('/auth/activate/:link', UserController.activate);
 router.get('/auth/refresh', UserController.refresh);
 
-router.get('/auth/users', UserController.getUsers);
+router.get('/auth/users', authMiddleware ,UserController.getUsers);
 
 module.exports = router;
