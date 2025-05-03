d2c0b0708c3daa2d981010add808f8061930f7b2
Illya <illakoroluk62@gmail.com>
2025-05-03 19:28:20 +0300

    complete index.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, {createContext} from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Store from "./store/store";
+import './index.css';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+interface State {
+    store: Store,
+}
+
+export const store = new Store();
+
+export const Context = createContext<State>({
+    store,
+})
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Context.Provider value={{
+        store
+    }}>
+        <App />
+    </Context.Provider>
 );
